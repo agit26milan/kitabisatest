ffdfd212ece2044ca5979d004fe1b29951313832
agitafirstawan <agitafirstawan26@gmail.com>
2022-04-24 18:13:47 +0700

    update type

--- pages/api/v1/campaigns.ts
+++ pages/api/v1/campaigns.ts
@@ -1,35 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import axios from 'axios'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { CampaignProps } from '../../../interfaces/campaigns'
 
 type Data = {
   success: boolean,
   data: object,
   message: string
-}
-
-type FetchDataProps = {
-  campaigner: string,
-  campaigner_badge: string,
-  campaigner_is_verified: boolean,
-  campaigner_type: string,
-  category_name: string,
-  custom_fb_pixel: string,
-  days_remaining: number,
-  donation_percentage: number,
-  donation_received: number,
-  donation_target: number,
-  expired: number,
-  id: number,
-  image: string,
-  is_featured: number,
-  is_forever_running: boolean,
-  is_open_goal: boolean,
-  order: number,
-  parent_project_id: number,
-  request_userdata: boolean,
-  short_url: string,
-  title: string
 }
 
 const url = "https://storage.googleapis.com/southern-waters-642.appspot.com/fe_code_challenge/campaign.json"
@@ -49,16 +26,16 @@ export default async function handler(
     let sortData = fetchData.data && fetchData.data.data
     if(sortData && Array.isArray(sortData)) {
       if(query.sort === DONATION_ASC) {
-        sortData = sortData.sort((a:FetchDataProps, b: FetchDataProps) => b.donation_target - a.donation_target)
+        sortData = sortData.sort((a:CampaignProps, b: CampaignProps) => b.donation_target - a.donation_target)
       }
       if(query.sort === DONATION_DSC) {
-        sortData = sortData.sort((a: FetchDataProps, b: FetchDataProps) => a.donation_target - b.donation_target)
+        sortData = sortData.sort((a: CampaignProps, b: CampaignProps) => a.donation_target - b.donation_target)
       }
       if(query.sort === EXPIRED_ASC) {
-        sortData = sortData.sort((a: FetchDataProps, b: FetchDataProps) => b.expired - a.expired)
+        sortData = sortData.sort((a: CampaignProps, b: CampaignProps) => b.expired - a.expired)
       }
       if(query.sort === EXPIRED_DSC) {
-        sortData = sortData.sort((a: FetchDataProps, b: FetchDataProps) => a.expired - b.expired)
+        sortData = sortData.sort((a: CampaignProps, b: CampaignProps) => a.expired - b.expired)
       }
     }
     response = {...response, data: sortData}
